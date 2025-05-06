f52a2f94e827a122a423950d4bf32425cb5378c2
dariiariabtseva <ryabtseva.dar@gmail.com>
2025-05-06 11:51:28 +0100

    version

--- script.js
+++ script.js
@@ -2,3 +2,24 @@ function toggleMenu() {
     const navbar = document.getElementById('navbar');
     navbar.classList.toggle('active');  // Toggle the 'active' class
 }
+
+
+    const targetDate = new Date("September 15, 2025 00:00:00").getTime();
+
+    const countdownInterval = setInterval(function() {
+        const now = new Date().getTime();
+        const timeRemaining = targetDate - now;
+
+        const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+
+        document.getElementById("countdown").innerHTML =
+            days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+        if (timeRemaining < 0) {
+            clearInterval(countdownInterval);
+            document.getElementById("countdown").innerHTML = "The special offer has ended!";
+        }
+    }, 1000);
